002903b63e830275a6b143ef7747d4a57a9f36a9
Habiba Sultana Bristy <bristysultana4@gmail.com>
2026-04-16 19:43:18 +0600

    banner added

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Banner from "@/components/homePage/banner/Banner";
+import YourFriends from "@/components/yourFriends/YourFriends";
+
+
+export default function Home() {
+  return (
+    <>
+      <Banner />
+      <YourFriends/>
+    </>
+  );
+}
