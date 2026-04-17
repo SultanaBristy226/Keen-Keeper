a0812c35d5fc5114a09ff2e312235bcbccdfeaf4
Habiba Sultana Bristy <bristysultana4@gmail.com>
2026-04-17 15:41:01 +0600

    fix error

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
 import Banner from "@/components/homePage/banner/Banner";
 import YourFriends from "@/components/yourFriends/YourFriends";
-import { getUsers } from "../lib/data";
 
 export default async function Home() {
-  const users = await getUsers();
-
   return (
     <>
       <Banner />
-      <YourFriends users={users} />
+      <YourFriends users={[]} />
     </>
   );
 }
--- src/lib/contexts/data.js
+++
@@ -1,7 +0,0 @@
-
-export async function getUsers() {
-  return [
-    { id: 1, name: "Bristy" },
-    { id: 2, name: "Khajida" }
-  ];
-}
